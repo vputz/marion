7d95bfe64637a27aa832b1f1bcdaf5326dce663c
vputz <vputz@nyx.net>
2014-10-16 23:45:46 +0000

    added basic graphs

---
+++ tutorial/static/javascript/limitController.js
@@ -0,0 +1,12 @@
+// an attempt to make a "limit controller" to control 
+// the maxItem variable of a bar chart.
+
+function maxItemController() {
+
+    function controller( chart ) {
+	
+    }
+
+    return controller;
+
+}
