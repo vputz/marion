b90a32fef5d24f0ca609fc1951d7d909f9a985c4
vputz <vputz@nyx.net>
2015-08-26 21:15:25 +0100

    changes to db creation

--- tutorial_db_create.py
+++ tutorial_db_create.py
@@ -19,7 +19,15 @@ def replace_query( db, name, description, filename, template ):
     for q in existing:
         db.session.delete(q)
     newq = Query(name=name, description=description, filename=filename, template=template)
-    db.session.add(q)
+    db.session.add(newq)
+    db.session.commit()
+
+def replace_tabdata_query(db, name, description, parameters, template):
+    existing = Tabdata_query.query.filter_by(name=name)
+    for q in existing:
+        db.session.delete(q)
+    newq = Tabdata_query(name=name, description=description, parameters=parameters, template=template)
+    db.session.add(newq)
     db.session.commit()
 
 with app.app_context() :
@@ -27,15 +35,13 @@ with app.app_context() :
     db.session.commit()
 
     # create queries
-    q = replace_query(db, name = "Subject", description = "Analysis of a subject; most-published journals, authors, universities, countries", filename = "subject_query.json", template="subject_query.html" )
-    q = replace_query(db, name = "Country", description = "Analysis of a country; number of papers by subject, university, collaborating country", filename = "country_query.json", template="country_query.html" )
-    q = replace_query(db, name = "Hexbin", description = "Hexbin-and-arcs display of papers in a dataset; keep sets small!", filename = "hexbin_query.json", template="hexbin_query.html" )
+    replace_query(db, name = "Subject", description = "Analysis of a subject; most-published journals, authors, universities, countries", filename = "subject_query.json", template="subject_query.html" )
+    replace_query(db, name = "Country", description = "Analysis of a country; number of papers by subject, university, collaborating country", filename = "country_query.json", template="country_query.html" )
+    replace_query(db, name = "Hexbin", description = "Hexbin-and-arcs display of papers in a dataset; keep sets small!", filename = "hexbin_query.json", template="hexbin_query.html" )
 
     # create tabdata queries
-    q = Tabdata_query( name="Tabdata_hexbin", description="Hexbin of geographical data", parameters='{ "locationColumn" : 0, "valueColumn" : 0, "textColumn" : 0 }', template="tabdata_hexbin_query.html" )
-    q = Tabdata_query( name="Tabdata_markercluster", description="Markercluster map of geographical data", parameters='{ "locationColumn" : 0, "valueColumn" : 0, "textColumn" : 0 }', template="tabdata_markercluster_query.html" )
-    db.session.add(q)
-    db.session.commit()
+    replace_tabdata_query( name="Tabdata_hexbin", description="Hexbin of geographical data", parameters='{ "locationColumn" : 0, "valueColumn" : 0, "textColumn" : 0 }', template="tabdata_hexbin_query.html" )
+    replace_tabdata_query( name="Tabdata_markercluster", description="Markercluster map of geographical data", parameters='{ "locationColumn" : 0, "valueColumn" : 0, "textColumn" : 0 }', template="tabdata_markercluster_query.html" )
 
         # create users
     user_datastore = SQLAlchemyUserDatastore( db, User, Role )
